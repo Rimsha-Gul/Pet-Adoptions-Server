76a5c47c610cdd4a5ca920cf4ad8901fdef6d51e
Rimsha-Gul-TH <rimsha@tetrahex.com>
2023-05-10 16:52:54 +0500

    Configure JOI validation

--- src/routes/session.ts
+++ src/routes/session.ts
@@ -1,13 +1,16 @@
 import express from "express";
 import { SessionController } from "../controllers/session";
 import { authenticateAccessToken } from "../middleware/authenticateToken";
+import { sessionValidation } from "../utils/validation";
 
 const sessionRouter = express.Router();
 
 sessionRouter.get("/", authenticateAccessToken, async (req, res) => {
+  const { error, value: body } = sessionValidation(req.body);
+  if (error) return res.status(400).send(error.details[0].message);
   const controller = new SessionController();
   try {
-    const response = await controller.session(req);
+    const response = await controller.session(body);
     res.send(response);
   } catch (err: any) {
     res.status(403).send(err.message);
