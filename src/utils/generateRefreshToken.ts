328720bfe1f0d514f02a8bc7ed9c23b1b301a1f9
Rimsha-Gul-TH <rimsha@tetrahex.com>
2023-05-12 19:51:10 +0500

    Fix lint warnings

--- src/utils/generateRefreshToken.ts
+++ src/utils/generateRefreshToken.ts
@@ -1,6 +1,6 @@
 import jwt from 'jsonwebtoken'
 
 export const generateRefreshToken = (email: string) =>
-  jwt.sign({ email }, process.env.REFRESH_TOKEN_SECRET!, {
+  jwt.sign({ email }, process.env.REFRESH_TOKEN_SECRET || 'default-secret', {
     expiresIn: '1d'
   })
