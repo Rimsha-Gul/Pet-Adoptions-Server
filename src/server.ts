bd2e57b8095217e9ef7c773cb007ec30afe9f8c2
Rimsha-Gul-TH <rimsha@tetrahex.com>
2023-11-03 16:41:48 +0500

    Update server.ts

--- src/server.ts
+++ src/server.ts
@@ -5,7 +5,10 @@ import { io } from './socket'
 dbPromise?.then(() => {
   try {
     const port = process.env.PORT || 8080
-    const appURL = process.env.WEB_APP_URL
+    const backendURL =
+      process.env.NODE_ENV === 'production'
+        ? `${process.env.SERVER_URL}`
+        : `${process.env.SERVER_URL}:${port}`
 
     // Create HTTP server
     const httpServer = createServer(app)
@@ -14,7 +17,7 @@ dbPromise?.then(() => {
     io.attach(httpServer)
 
     httpServer.listen(port, () => {
-      console.log(`⚡️[server]: Server is running at ${appURL}:${port}`)
+      console.log(`⚡️[server]: Server is running at ${backendURL}`)
     })
   } catch (err) {
     console.error('Error starting the server: ', err)
